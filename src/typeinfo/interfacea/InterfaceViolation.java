02147899ead9139ed1c5301e8c4c06fe101a82be
kaka2634 <996529090@qq.com>
2019-12-09 23:08:56 +0800

    learn how reflection violate the encapsulation

---
+++ src/typeinfo/interfacea/InterfaceViolation.java
@@ -0,0 +1,25 @@
+package typeinfo.interfacea;
+
+
+class B implements A {
+
+    @Override
+    public void f() {
+    }
+
+    public void g() {
+    }
+}
+
+public class InterfaceViolation {
+    public static void main(String[] args) {
+        A a = new B();
+        a.f();
+        // a.g(); // Compiler error
+        System.out.println(a.getClass().getName());
+        //downcast to B, interface A cannot protect the legal exposed method now
+        B b = (B) a;
+        b.g();
+    }
+
+}
