1d0da72f377d0756b90b848d5dc755c1a2e78e6b
kaka2634 <996529090@qq.com>
2019-12-11 23:50:34 +0800

    learn array of generic

--- src/arrays/ArrayOfGenerics.java
+++ src/arrays/ArrayOfGenerics.java
@@ -29,12 +29,33 @@ public class ArrayOfGenerics {
         //List<String>[] is a subtype of Object[]
         Object[] objects = ls;
 
+
+        /*** Array vs Generic List ***/
+
         //Because ArrayList<Integer> is subtype of Object
         objects[1] = new ArrayList<Integer>();
-
-        //Can put different type in object[1] 
+        //Can put different type in List, object[1] is List<Integer>
+        //If fool compiler then can fool run-time
         ((ArrayList<Integer>)objects[1]).add(1);
-        ((ArrayList<ArrayOfGenerics>)objects[1]).add(new ArrayOfGenerics());
+        ((ArrayList<ArrayOfGenerics>)objects[1]).add(new ArrayOfGenerics());//heap polution
         System.out.println(objects[1]);
+
+
+        //Array can fool compiler but cannot fool run-time type system
+        Integer[] myInts = {1,2,3,4};
+        Number[] myNumber = myInts;
+        //ArrayStoreException
+        myNumber[0] = 3.14; //attempt of heap pollution
+
+        //If Generic List can fool compiler, then it can fool run-time type system
+        Object myInts2 = new ArrayList<Integer>();
+        //Compiler can
+        (((ArrayList<Integer>)myInts2)).add(1);
+        (((ArrayList<Double>)myInts2)).add(3.14); //heap polution
+        System.out.println(myInts2);
+
+        List<Integer> myInts3 = new ArrayList<Integer>();
+        //Compiler will prevent this cast
+        //((ArrayList<Double>)myInts2))myInts3
     }
 }
