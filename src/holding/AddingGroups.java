38f6396512d15301e6810f5cd4fa1849fea0f52b
kaka2634 <996529090@qq.com>
2019-12-15 00:25:34 +0800

    learn add group of elements

---
+++ src/holding/AddingGroups.java
@@ -0,0 +1,29 @@
+package holding;
+
+//Adding groups of elements to Collection objects
+
+import java.util.*;
+
+public class AddingGroups {
+    public static void main(String[] args) {
+
+        //Arrays.asList return Collection
+        Collection<Integer> collection = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+
+        //Collection.addAll(collection)
+        Integer[] moreInts = {6,7,8,9,10};
+        collection.addAll(Arrays.asList(moreInts));
+
+        //Collections.addAll(collection, ...  )
+        //Runs significantly faster, but you can't construct a Collection this way
+        Collections.addAll(collection, 11,12,13,14,15);
+
+        /* !Arrays.asList return list cannot change the length */
+        List<Integer> list = Arrays.asList(16,17,18,19,20);
+        list.set(1,2); //OK -- modify an element
+        System.out.println(list);
+        list.add(21); //Runtime error: UnsupportedOperationException
+                      // because the underlying array cannot be resized
+        System.out.println(list);
+    }
+}
