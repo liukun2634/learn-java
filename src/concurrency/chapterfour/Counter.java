c04550a1daa4a0875023ce7330f829fd37138a7c
kaka2634 <996529090@qq.com>
2020-08-23 23:31:03 +0800

    add concurrentcy chapter four code

---
+++ src/concurrency/chapterfour/Counter.java
@@ -0,0 +1,22 @@
+package concurrency.chapterfour;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class Counter {
+    @GuardedBy("this")
+    private long value = 0;
+
+    public synchronized long getValue() {
+        return value;
+    }
+
+    public synchronized long increment() {
+        //Consider overflow
+        if (value == Long.MAX_VALUE) {
+            throw new IllegalStateException("counter overflow");
+        }
+        return ++value;
+    }
+}
