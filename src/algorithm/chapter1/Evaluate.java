c1835c2ed0e48d7752aed21b885b1b9822cbc72c
kaka2634 <996529090@qq.com>
2020-11-16 00:12:38 +0800

    add class evaluate

---
+++ src/algorithm/chapter1/Evaluate.java
@@ -0,0 +1,36 @@
+package algorithm.chapter1;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Evaluate {
+    public static void main(String[] args)  {
+        Stack<String> operators = new Stack<>();
+        Stack<Double> values = new Stack<>();
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] strs= str.split(" ");
+        for (String s : strs) {
+            if (s.equals("("))                 ;
+            else if (s.equals("+")) operators.push(s);
+            else if (s.equals("-")) operators.push(s);
+            else if (s.equals("*")) operators.push(s);
+            else if (s.equals("/")) operators.push(s);
+            else if (s.equals("sqrt")) operators.push(s);
+            else if (s.equals(")"))
+            {
+                String operator = operators.pop();
+                double value = values.pop();
+                if (operator.equals("+")) value = values.pop() + value;
+                else if (operator.equals("-")) value = values.pop() - value;
+                else if (operator.equals("*")) value = values.pop() * value;
+                else if (operator.equals("/")) value = values.pop() / value;
+                else if (operator.equals("sqrt")) value =  Math.sqrt(value);
+                values.push(value);
+            }
+            else values.push(Double.parseDouble(s));
+        }
+        System.out.println(values.pop());
+    }
+}
