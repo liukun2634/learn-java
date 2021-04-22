b77c3d7777ed881c03e1625129fc7e13c435cb58
kaka2634 <996529090@qq.com>
2021-04-22 23:46:33 +0800

    add throw two eggs code

---
+++ src/algorithm/ThrowTwoEggs.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+public class ThrowTwoEggs {
+
+    //Two eggs,
+    //To find from which floor cloud break the egg
+    public static void main(String[] args) {
+        int floorNumber = 100;
+        int[] throwTimes = new int[floorNumber + 1] ;
+        throwTimes[0] = 0;
+        throwTimes[1] = 1;
+        throwTimes[2] = 1;
+        for(int i = 3; i <= floorNumber; i++) {
+            throwTimes[i] = floorNumber;
+        }
+
+        for(int i = 3; i <= floorNumber; i++) {
+            for (int k = 1; k  <= i; k++){
+                throwTimes[i] = Math.min(throwTimes[i], Math.max(k-1, throwTimes[i-k]) + 1);
+            }
+        }
+
+        for(int i = 0; i < floorNumber; i++) {
+           System.out.println(throwTimes[i]);
+        }
+    }
+}
